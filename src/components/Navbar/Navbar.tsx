ab7e1094dc6a5f716c02e57d89dca3ac471ea30b
Ian <iancheif117@gmail.com>
2024-09-08 17:13:43 +0800

    Fixing Build Errors

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 
